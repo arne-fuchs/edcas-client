d3e5a20000242099cc7e9475ccb0c98089f79fa3
arne-fuchs <arnefuchs@protonmail.com>
2023-06-13 19:37:08 +0200

    Added windows build script

---
+++ create-windows-release.bat
@@ -0,0 +1,38 @@
+@echo off
+
+setlocal
+
+set "folder_name=edcas-client"
+
+rem Clean build artifacts
+cargo clean
+
+rem Update dependencies
+cargo update
+
+rem Build release version
+cargo build --release
+
+rem Check if the folder exists
+if exist "%folder_name%" (
+    echo Folder '%folder_name%' found. Removing...
+    rem Remove the folder
+    rmdir /s /q "%folder_name%"
+    echo Folder removed.
+)
+
+rem Create the folder and subdirectories
+mkdir "%folder_name%"
+mkdir "%folder_name%\logs"
+
+rem Copy files and directories
+xcopy /E graphics "%folder_name%\graphics"
+copy settings-example.json "%folder_name%\settings-example.json"
+copy settings-example.json "%folder_name%\settings.json"
+copy start.sh "%folder_name%\"
+copy target\release\edcas-client.exe "%folder_name%\"
+
+rem Create ZIP archive
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip "%folder_name%.zip" "%folder_name%\*"
+
+endlocal
