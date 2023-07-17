90d6562d7c04d35119cc64656cde82b7be46ac5f
arne-fuchs <arnefuchs@protonmail.com>
2023-07-17 19:09:24 +0200

    Updated release script to provide some more information

--- create-linux-release.sh
+++ create-linux-release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Running cargo commands"
 cargo clean
 cargo update
 cargo build --release
@@ -12,9 +13,11 @@ if [ -d "$folder_name" ]; then
     rm -rf "$folder_name"
     echo "Folder removed."
 fi
-
+echo "Creating folders"
 mkdir "$folder_name"
 mkdir "$folder_name"/logs
+
+echo "Copying files"
 cp -r graphics "$folder_name"/graphics
 cp settings-example.json "$folder_name"/settings-example.json
 cp settings-example.json "$folder_name"/settings.json
@@ -22,4 +25,5 @@ cp materials.json "$folder_name"/materials.json
 cp start.sh "$folder_name"/
 cp target/release/edcas-client "$folder_name"/
 
+echo "Compressing files"
 tar czf edcas-client.tar.gz "$folder_name"
