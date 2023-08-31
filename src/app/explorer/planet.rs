16228a1a6dff2e45c2df4e8431537ed881d720a6
arne-fuchs <arnefuchs@protonmail.com>
2023-08-31 12:28:02 +0200

    Fixed wrong mapped display

--- src/app/explorer/planet.rs
+++ src/app/explorer/planet.rs
@@ -251,7 +251,7 @@ impl BodyImplementation for Planet {
             ui.label("|");
             ui.label(self.settings.icons.get("discovered").unwrap().get_richtext());
         }
-        if self.was_discovered && self.settings.icons.get("mapped").unwrap().enabled{
+        if self.was_mapped && self.settings.icons.get("mapped").unwrap().enabled{
             ui.label("|");
             ui.label(self.settings.icons.get("mapped").unwrap().get_richtext());
         }
