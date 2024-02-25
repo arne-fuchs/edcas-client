3dcd170f10d98765960fa30b89204c0bb7376511
Nannk <nannk@cock.li>
2024-02-25 20:34:25 +0100

    materials tab layout

--- src/tui.rs
+++ src/tui.rs
@@ -13,6 +13,8 @@ struct App<'a> {
     pub titles: Vec<&'a str>,
     pub tab_index: usize,
     pub body_list_state: ListState,
+    pub material_index: usize,
+    pub material_list_state: ListState,
 }
 
 impl<'a> App<'a> {
@@ -21,6 +23,8 @@ impl<'a> App<'a> {
             titles: vec!["Explorer", "Mining", "Materials", "About"],
             tab_index: 0,
             body_list_state: ListState::default(),
+            material_index: 0,
+            material_list_state: ListState::default(),
         }
     }
 
@@ -37,7 +41,9 @@ impl<'a> App<'a> {
     }
 
     pub fn next_system(&mut self, client: &mut EliteRustClient) {
-        if client.explorer.index + 1 < client.explorer.systems.len() {
+        if !client.explorer.systems.is_empty()
+            && client.explorer.index + 1 < client.explorer.systems.len()
+        {
             client.explorer.index += 1;
         }
     }
@@ -49,26 +55,34 @@ impl<'a> App<'a> {
     }
 
     pub fn next_body(&mut self, client: &mut EliteRustClient) {
-        if client.explorer.systems[client.explorer.index].index + 1
-            < client.explorer.systems[client.explorer.index]
-                .body_list
-                .len()
+        if !client.explorer.systems.is_empty()
+            && client.explorer.systems[client.explorer.index].index + 1
+                < client.explorer.systems[client.explorer.index]
+                    .body_list
+                    .len()
+        // TODO: fix: crashes the tui if the systems list is empty
         {
             client.explorer.systems[client.explorer.index].index += 1;
         }
     }
 
     pub fn previous_body(&mut self, client: &mut EliteRustClient) {
-        if !client.explorer.systems[client.explorer.index]
-            .body_list
-            .is_empty()
+        if !client.explorer.systems.is_empty()
             && client.explorer.systems[client.explorer.index].index > 0
         {
             client.explorer.systems[client.explorer.index].index -= 1;
         }
     }
 
-    // TODO: add functions for cursor navigation through signals/bodies lists
+    pub fn next_material(&mut self, client: &mut EliteRustClient) {
+        // TODO:
+    }
+
+    pub fn previous_material(&mut self, client: &mut EliteRustClient) {
+        // TODO:
+    }
+
+    // TODO: add functions for cursor navigation through signals lists
 }
 
 pub fn draw_tui(client: EliteRustClient) -> Result<(), Box<dyn Error>> {
@@ -118,8 +132,16 @@ fn run_app<B: Backend>(
                         KeyCode::Char('q') => app.previous_tab(),
                         KeyCode::Right => app.next_system(&mut client),
                         KeyCode::Left => app.previous_system(&mut client),
-                        KeyCode::Down => app.next_body(&mut client),
-                        KeyCode::Up => app.previous_body(&mut client),
+                        KeyCode::Down => match app.tab_index {
+                            0 => app.next_body(&mut client),
+                            2 => app.next_material(&mut client),
+                            _ => {}
+                        },
+                        KeyCode::Up => match app.tab_index {
+                            0 => app.previous_body(&mut client),
+                            2 => app.previous_material(&mut client),
+                            _ => {}
+                        },
                         // TODO: add keys for cursor navigation through signals list
                         _ => {}
                     }
@@ -150,7 +172,7 @@ fn ui(f: &mut Frame, app: &mut App, client: &EliteRustClient) {
         )
         .select(app.tab_index)
         .style(Style::default().white())
-        .highlight_style(Style::default().bold().white().on_gray());
+        .highlight_style(Style::default().bold().white().on_dark_gray());
     f.render_widget(tabs, tabs_and_timestamp[0]);
 
     let timestamp = Paragraph::new(client.timestamp.clone())
@@ -162,7 +184,7 @@ fn ui(f: &mut Frame, app: &mut App, client: &EliteRustClient) {
     match app.tab_index {
         0 => tab_explorer(chunks[1], f, client, app),
         1 => tab_mining(chunks[1], f, client, app),
-        2 => tab_materials(chunks[1], f, client),
+        2 => tab_materials(chunks[1], f, client, app),
         3 => tab_about(chunks[1], f),
         _ => unreachable!(),
     };
@@ -170,24 +192,15 @@ fn ui(f: &mut Frame, app: &mut App, client: &EliteRustClient) {
 
 // ======== Tabs functions =========
 
-fn tab_default(chunk: ratatui::layout::Rect, f: &mut ratatui::Frame, _client: &EliteRustClient) {
-    let widget_default = Paragraph::new("default text here").block(
-        Block::default()
-            .title("Default")
-            .borders(Borders::ALL)
-            .white(),
-    );
-
-    f.render_widget(widget_default, chunk);
-}
-
+// Explorer --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 fn tab_explorer(
     chunk: ratatui::layout::Rect,
     f: &mut ratatui::Frame,
     client: &EliteRustClient,
     app: &mut App,
 ) {
-    //general layout
+    // Layout definitions
+    // general layout
     let layout_explorer = ratatui::prelude::Layout::default()
         .direction(ratatui::prelude::Direction::Horizontal)
         .constraints(vec![
@@ -197,16 +210,27 @@ fn tab_explorer(
         ])
         .split(chunk);
 
+    // layout of "systems" Panel
     let layout_system = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Length(11), Constraint::Fill(1)])
         .split(layout_explorer[0]);
 
+    // layout of "body information" panel
+    let layout_body = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(20), Constraint::Fill(1)])
+        .split(layout_explorer[2]);
+    // Data
+    // Default data to display
     let mut data_system_info = vec!["no data".to_string()];
     let mut data_signals_list = vec!["no data".to_string()];
     let mut data_body_list = vec!["no data".to_string()];
     let mut data_body_signals_list = vec!["no data".to_string()];
+    let mut data_body_info = vec!["no data".to_string()];
 
+    // Checks to not crash everything if list is empty
+    // Data acquisition
     if !client.explorer.systems.is_empty() {
         data_system_info = vec![
             client.explorer.systems[client.explorer.index].name.clone(),
@@ -252,9 +276,9 @@ fn tab_explorer(
                     let mut space_string = "".to_string();
                     for i in 0..body.get_parents().len() {
                         if i < body.get_parents().len() - 1 {
-                            space_string.push_str("| ")
+                            space_string.push_str("│  ")
                         } else {
-                            space_string.push_str("|-");
+                            space_string.push_str("│  "); //├─
                         }
                     }
                     space_string.push_str(body.get_name());
@@ -262,19 +286,15 @@ fn tab_explorer(
                 })
                 .collect::<Vec<_>>();
 
-            //Selection from body_list
+            //TODO: parse json to Vec and use it here
+            data_body_info = vec![client.explorer.systems[client.explorer.index].body_list
+                [client.explorer.systems[client.explorer.index].index]
+                .get_name()
+                .to_string()];
+
+            // Selection from body_list (cursor and scrolling)
             app.body_list_state
                 .select(Some(client.explorer.systems[client.explorer.index].index));
-
-            /*
-                        if client.explorer.systems[client.explorer.index].index
-                            > usize::from(layout_explorer[1].height)
-                        {
-                            let needed_offset = client.explorer.systems[client.explorer.index].index
-                                - usize::from(layout_explorer[1].height);
-                            *app.body_list_state.offset_mut() = needed_offset;
-                        }
-            */
 
             if !client.explorer.systems[client.explorer.index].body_list
                 [client.explorer.systems[client.explorer.index].index]
@@ -299,34 +319,52 @@ fn tab_explorer(
         }
     }
 
+    // Widget definitions
     let widget_system_info = List::new(data_system_info).block(
         Block::default()
             .title("System Info")
-            .borders(Borders::ALL)
+            .borders(Borders::TOP)
+            .bold()
             .white(),
     );
-    f.render_widget(widget_system_info, layout_system[0]);
 
-    //widget for signals list
     let widget_signal_list = List::new(data_signals_list).block(
         Block::default()
             .title("Signals")
-            .borders(Borders::ALL)
+            .borders(Borders::TOP)
+            .bold()
             .white(),
     );
 
-    f.render_widget(widget_signal_list, layout_system[1]);
-
-    //widget for bodies list
-    // TODO: automatical generation of sub-layouts for body features like rings or planets
     let widget_body_list = List::new(data_body_list)
         .block(
             Block::default()
                 .title("Body List")
-                .borders(Borders::ALL)
+                .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
+                .bold()
                 .white(),
         )
-        .highlight_style(Style::default().white().on_gray());
+        .highlight_style(Style::default().bold().white().on_dark_gray());
+
+    let widget_body_info = List::new(data_body_info).block(
+        Block::default()
+            .title("Body Info")
+            .borders(Borders::TOP)
+            .bold()
+            .white(),
+    );
+
+    let widget_body_signals_list = List::new(data_body_signals_list).block(
+        Block::default()
+            .title("Body Signals")
+            .borders(Borders::TOP)
+            .bold()
+            .white(),
+    );
+
+    // render calls
+    f.render_widget(widget_system_info, layout_system[0]);
+    f.render_widget(widget_signal_list, layout_system[1]);
 
     f.render_stateful_widget(
         widget_body_list,
@@ -334,31 +372,138 @@ fn tab_explorer(
         &mut app.body_list_state,
     );
 
-    let widget_body_signals_list = List::new(data_body_signals_list).block(
-        Block::default()
-            .title("Body Signals")
-            .borders(Borders::ALL)
-            .white(),
-    );
-
-    f.render_widget(widget_body_signals_list, layout_explorer[2]);
+    f.render_widget(widget_body_info, layout_body[0]);
+    f.render_widget(widget_body_signals_list, layout_body[1]);
 }
+
+// Mining --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 fn tab_mining(
     chunk: ratatui::layout::Rect,
     f: &mut ratatui::Frame,
     client: &EliteRustClient,
-    app: &App,
+    app: &mut App,
 ) {
     //TODO:
 }
 
-fn tab_materials(chunk: ratatui::layout::Rect, f: &mut ratatui::Frame, client: &EliteRustClient) {
+// Materials --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+fn tab_materials(
+    chunk: ratatui::layout::Rect,
+    f: &mut ratatui::Frame,
+    client: &EliteRustClient,
+    app: &mut App,
+) {
     //TODO:
+
+    // Layout definitions
+    let layout_materials = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Length(45), Constraint::Fill(1)])
+        .split(chunk);
+
+    let layout_materials_list = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Length(36), Constraint::Fill(1)])
+        .split(layout_materials[0]);
+
+    let layout_materials_info = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Fill(1), // Description
+            Constraint::Fill(1), // Location
+            Constraint::Fill(1), // Sources
+            Constraint::Fill(1), // Engineering
+            Constraint::Fill(1), // Synthesis
+        ])
+        .split(layout_materials[1]);
+
+    // TODO: data acquisition
+
+    // Selection from materials list (cursor and scrolling)
+    app.material_list_state.select(Some(app.material_index));
+
+    // Widget definitions
+    // material_list field
+    let widget_materials_list_names = List::new(["material 1", "material 2", "material 3"])
+        .block(
+            Block::default()
+                .title("Materials")
+                .borders(Borders::TOP)
+                .white(),
+        )
+        .highlight_style(Style::default().bold().white().on_dark_gray());
+
+    let widget_materials_list_count = List::new(["100/200", "10/250", "0/100"])
+        .block(Block::default().borders(Borders::TOP | Borders::RIGHT));
+
+    // materials_info
+    let widget_materials_info_description =
+        Paragraph::new("description depending on selected material")
+            .wrap(Wrap { trim: true })
+            .block(
+                Block::default()
+                    .title("Description")
+                    .bold()
+                    .borders(Borders::TOP),
+            );
+    let widget_materials_info_location =
+        Paragraph::new("location info depending on selected material")
+            .wrap(Wrap { trim: true })
+            .block(
+                Block::default()
+                    .title("Location")
+                    .bold()
+                    .borders(Borders::TOP),
+            );
+    let widget_materials_info_source =
+        Paragraph::new("sources info depending on selected material")
+            .wrap(Wrap { trim: true })
+            .block(
+                Block::default()
+                    .title("Sources")
+                    .bold()
+                    .borders(Borders::TOP),
+            );
+    let widget_materials_info_engineering =
+        Paragraph::new("engineering info depending on selected material")
+            .wrap(Wrap { trim: true })
+            .block(
+                Block::default()
+                    .title("Engineering")
+                    .bold()
+                    .borders(Borders::TOP),
+            );
+    let widget_materials_info_synthesis =
+        Paragraph::new("synthesis info depending on selected material")
+            .wrap(Wrap { trim: true })
+            .block(
+                Block::default()
+                    .title("Synthesis")
+                    .bold()
+                    .borders(Borders::TOP),
+            );
+
+    // Render calls
+    f.render_stateful_widget(
+        widget_materials_list_names,
+        layout_materials_list[0],
+        &mut app.material_list_state,
+    );
+    f.render_widget(widget_materials_list_count, layout_materials_list[1]);
+
+    f.render_widget(widget_materials_info_description, layout_materials_info[0]);
+    f.render_widget(widget_materials_info_location, layout_materials_info[1]);
+    f.render_widget(widget_materials_info_source, layout_materials_info[2]);
+    f.render_widget(widget_materials_info_engineering, layout_materials_info[3]);
+    f.render_widget(widget_materials_info_synthesis, layout_materials_info[4]);
 }
 
+// About --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 fn tab_about(chunk: ratatui::layout::Rect, f: &mut ratatui::Frame) {
-    // ob ich den layout überhaupt brauhe?
+    // Layout definition
     let layout_about = ratatui::prelude::Layout::default()
         .direction(ratatui::prelude::Direction::Vertical)
         .constraints(vec![
@@ -368,29 +513,35 @@ fn tab_about(chunk: ratatui::layout::Rect, f: &mut ratatui::Frame) {
         ])
         .split(chunk);
 
-    let widget_about_github = Paragraph::new("https://github.com/arne-fuchs/edcas-client").block(
-        Block::default()
-            .title("GitHub")
-            .borders(Borders::ALL)
-            .white(),
-    );
-    f.render_widget(widget_about_github, layout_about[1]);
+    // Widget definitions
+    let widget_about_github = Paragraph::new("https://github.com/arne-fuchs/edcas-client")
+        .wrap(Wrap { trim: true })
+        .block(
+            Block::default()
+                .title("GitHub")
+                .borders(Borders::TOP)
+                .white(),
+        );
 
     let widget_about_version = Paragraph::new(env!("CARGO_PKG_VERSION")).block(
         Block::default()
             .title("edcas version")
-            .borders(Borders::ALL)
+            .borders(Borders::TOP)
             .white(),
     );
-    f.render_widget(widget_about_version, layout_about[2]);
 
     let widget_about_controls =
-        Paragraph::new("Quit: Q, Change Tabs: q and e, Change System: Left and Right arrows")
+        Paragraph::new("Quit: Q, Change Tabs: q and e, Change System: Left and Right arrows, Change item selection: Up and Down arrows")
+            .wrap(Wrap { trim: true })
             .block(
                 Block::default()
                     .title("Controls")
-                    .borders(Borders::ALL)
+                    .borders(Borders::TOP)
                     .white(),
             );
+
+    // Render calls
+    f.render_widget(widget_about_github, layout_about[1]);
+    f.render_widget(widget_about_version, layout_about[2]);
     f.render_widget(widget_about_controls, layout_about[0]);
 }
