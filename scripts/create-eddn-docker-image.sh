8830f2b6e52a801dd81856ea4272af4d9a205ba1
arne-fuchs <arnefuchs@protonmail.com>
2024-08-08 11:25:35 +0200

    Changed tag name

--- scripts/create-eddn-docker-image.sh
+++ scripts/create-eddn-docker-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --tag edcas-client:eddn ./
-echo "Finished building image edcas-client:eddn"
+docker build --tag edcas-eddn:latest ./
+echo "Finished building image edcas-eddn:latest"
